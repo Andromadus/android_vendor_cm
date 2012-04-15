d531b2ffc8dafe7353877684867c1364b0a2dc49
Nicholas Flintham <nick@flinny.org>
2012-04-15 18:38:20 +0100

    Revert "Add common config for devices with low space on system"
    This reverts commit 686fc81cca57a22847b47c69472f7fcc6de84f05.

--- config/common.mk
+++ config/common.mk
@@ -46,7 +46,8 @@ PRODUCT_COPY_FILES +=  \
     vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm3.so:system/lib/libjackpal-androidterm3.so \
     vendor/cm/prebuilt/common/bootanimation.zip:system/media/bootanimation.zip
 
-# Bring in camera effects
+# Bring in camera effects & videos
+$(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
 PRODUCT_COPY_FILES +=  \
     vendor/cm/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
     vendor/cm/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
